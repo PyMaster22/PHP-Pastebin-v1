bd48cf6fc81b4bcac769fa79ff13571234ae23d1
PyMaster22 <59183571+PyMaster22@users.noreply.github.com>
2020-09-03 16:19:16 -0700

    Update new.php

--- new.php
+++ new.php
@@ -3,8 +3,20 @@ $paste_file = "pastes.php";
 if(!file_exists($paste_file)){
   file_put_contents($paste_file, "<?php header(\"content-type:text/plain\"); if($_GET[\"pasteid\"]==\"0000000000\"){echo \"Pastes.\";}");
 }
+function generateRandomString($length = 11) {
+  $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_';
+  $charactersLength = strlen($characters);
+  $randomString = '';
+  for ($i = 0; $i < $length; $i++) {
+    $randomString .= $characters[rand(0, $charactersLength - 1)];
+  }
+  return $randomString;
+}
 if($_POST){
-  
+  $pasteid = generateRandomString();
+  file_put_contents($paste_file, file_get_contents($paste_file) . "elseif(\$_GET[\"$pasteid\"]){echo \"$_POST["text2paste"]\";}");
+  echo "<a href=\"".$paste_file."?pasteid=".$pasteid."\">Your paste.</a>";
+  exit;
 }
 ?>
 <!DOCTYPE html>
@@ -17,6 +29,9 @@ if($_POST){
 </head>
 <body>
   <form action="" method="post">
+    <textarea cols="50" rows="10" placeholder="Paste Content..." name="text2paste"></textarea><br>
+    <p>BURN on view and disable listing comming later. :/</p>
+    <input type="submit" value="Post!">
   </form>
 </body>
 </html>
